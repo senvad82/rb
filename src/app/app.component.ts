ac9dc8086a1454406fa0e8fc05ed4b25b987b122
senthilvadivel venkatesan <senthilvadivel venkatesan>
2016-12-04 22:08:46 -0500

    recipe-book app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import {HeaderComponent} from './header.component'
+import {RecipesComponent} from './recipes/recipes.component'
 @Component({
   selector: 'rb-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  templateUrl: './app.component.html'
+  })
 export class AppComponent {
-  title = 'rb works!';
+  
 }
---
+++ src/app/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-header',
+  templateUrl: './header.component.html'
+ 
+})
+export class HeaderComponent {
+
+  
+}
